a7aaebd0509178ab048510b3561083d77012eed5
Juan <jaguado@iMac-de-Juan.local>
2019-01-12 18:21:50 +0100

    Init commit

---
+++ index.js
@@ -0,0 +1,66 @@
+export default class VueZeusWebsockets {
+    constructor(config) {
+        this.websocket = new WebSocket(config.connection);
+        this.events = {};
+    }
+
+    install(Vue) {
+        Vue.prototype.$socket = this;
+    }
+
+    onConnect(callback) {
+        this.websocket.onopen = () => {
+            this.websocket.onmessage = (e) => {
+                var data = JSON.parse(e.data);
+
+                if (this.events.hasOwnProperty(data.event)) {
+                    for (var fn in this.events[data.event]) {
+                        this.events[data.event][fn](data.message);
+                    }
+                }
+            };
+
+            callback();
+        }
+    }
+
+    join(channel) {
+        if (channel === undefined) {
+            throw new Error('You must provide a channel');
+        }
+
+        var data = {
+            action: 'join',
+            channel: channel
+        };
+
+        this.websocket.send(this.prepareData(data));
+    }
+
+    emit(event, message, channel) {
+        var data = {
+            action: 'message',
+            event: event,
+            message: message
+        };
+
+        if (channel !== undefined) {
+            data.channel = channel;
+        }
+
+        this.websocket.send(this.prepareData(data));
+    }
+
+    listen(event, callback) {
+        if (typeof callback !== 'function') {
+            throw new Error('Callback must be a function');
+        }
+
+        this.events[event] = this.events[event] = [];
+        this.events[event].push(callback);
+    }
+
+    prepareData(data) {
+        return JSON.stringify(data);
+    }
+}
